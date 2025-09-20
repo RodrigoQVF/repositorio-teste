22cfe47a9aafc9487bcf0fef99da9f8368cfb830
RodrigoQVF <rodrigoqvf10@gmail.com>
2025-09-20 18:25:53 -0300

    a

--- Polipet/src/Animal.java
+++ Polipet/src/Animal.java
@@ -2,6 +2,7 @@ public abstract class Animal {
 
     private String nome;
     private Double peso;
+    //COMENTARIOS FODAS PRA AJUDAR O OUTRO A LER O CODIGO
 
     public Animal(String nome, Double peso){
         this.nome = nome;
